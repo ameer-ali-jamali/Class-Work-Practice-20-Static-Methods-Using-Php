2f8547c625ba356a314423dc4f560866582810ae
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-09 19:27:44 +0500

    php Static Methods

--- index.php
+++ index.php
@@ -1,67 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
+Static Methods
+In certain cases where we need to access methods and properties in terms of class rather than an object, the static methods come for help. Any method declared as static can be accessed without creating an object.
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+Static methods are declared with the static keyword.
 
-<body>
-  <?php
-  //    Static Methods
-  //   In certain cases where we need to access methods and properties in terms of class rather than an object, the static methods come for help. Any method declared as static can be accessed without creating an object.
+Example
 
-  //  Static methods are declared with the static keyword.
 
-  // Example
 
-  class ClassName
+<?php
+class ClassName
+{
+  public static function staticMethod()
   {
-    public static function staticMethod()
-    {
-      echo "Hello World!";
-    }
+    echo "Hello World!";
   }
+}
+?>
 
 
-  //    To access a static method we need to use the class name followed by a double colon (::), and the method name.
+To access a static method we need to use the class name followed by a double colon (::), and the method name.
 
-  //   Example:
-
-
-  //  class greeting
-  // {
-  //   public static function welcome()
-  //   {
-  //     echo "Hello Bhai!";
-  //   }
-  // }
-
-  // Call static method
-  greeting::welcome();
+Example:
 
 
 
-  //    A static method can be accessed from a method in the same class using the self keyword followed by a double colon (::), and the method name.
-
-  //   Example:
-
-  class greeting
+<?php
+class greeting
+{
+  public static function welcome()
   {
-    public static function welcome()
-    {
-      echo "Hello Bro!";
-    }
-    public function __construct()
-    {
-      self::welcome();
-    }
+    echo "Hello Bhai!";
   }
-  new greeting();
+}
 
-  ?>
-</body>
+// Call static method
+greeting::welcome();
+?>
 
-</html>
+
+A static method can be accessed from a method in the same class using the self keyword followed by a double colon (::), and the method name.
+
+Example:
+
+<?php
+class greeeting
+{
+  public static function welcome()
+  {
+    echo "Hello Bro!";
+  }
+  public function __construct()
+  {
+    self::welcome();
+  }
+}
+new greeting();
+?>
